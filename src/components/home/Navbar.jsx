750100b654428c3bf43411c639fc98871655257d
mernking <davtekdon@gmail.com>
2024-05-05 19:23:25 +0100

    yes

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <header className="bg-background w-full py-4 px-3">
+    <header className="bg-background w-full py-4 px-3 flex justify-between items-center">
       <div className="logo">
         <h1>LCmass</h1>
       </div>
       <div className="navbtns">
-        <div className="desktop sm:block flex flex-row text-sm">
+        <div className="desktop hidden sm:block flex flex-row text-sm">
           <Link href="#hero">
             <p>Home</p>
           </Link>
@@ -25,7 +25,7 @@ export default function Navbar() {
             <p>Docs</p>
           </Link>
         </div>
-        <div className="mobile sm:hidden flex flex-col gap-2 px-2 py-2">
+        <div className="mobile w-full sm:hidden flex flex-col gap-2 px-2 py-2">
           <Link href="#hero">
             <p>Home</p>
           </Link>
@@ -40,6 +40,30 @@ export default function Navbar() {
           </Link>
           <Link href="#hero">
             <p>Docs</p>
+          </Link>
+          <div className="account_log flex flex-row">
+            <div className="login">
+              <Link href="/login">
+                <p>login</p>
+              </Link>
+            </div>
+            <div className="signup">
+              <Link href="/signup">
+                <p>sign up</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="account_log flex flex-row hidden sm:block">
+        <div className="login">
+          <Link href="/login">
+            <p>login</p>
+          </Link>
+        </div>
+        <div className="signup">
+          <Link href="/signup">
+            <p>sign up</p>
           </Link>
         </div>
       </div>
