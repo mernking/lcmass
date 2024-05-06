e3dd91aac2deeafec65355026d1acecd9200b3aa
mernking <davtekdon@gmail.com>
2024-05-06 11:53:48 +0100

    hello

--- models/user.js
+++ models/user.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = new mongoose.Schema({
-
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  since: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
--- utility/index.js
+++ utility/index.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const connection = await mongoose.connect("mongodb://localhost:27017/lcmass", {
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
   useNewUrlParser: true,
 });
 
